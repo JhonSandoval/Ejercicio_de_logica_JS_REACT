371544c12e8624caa96b54b4d57d4663c8ce640c
JhonSandoval <69171854+JhonSandoval@users.noreply.github.com>
2022-10-28 16:05:01 -0500

    Update parabola.js
    Actualizado ejercicio 2

--- ejercicio_Programación/parabola.js
+++ ejercicio_Programación/parabola.js
@@ -1,16 +1,7 @@
 // Ejercicios 2
-/*
-- definir una funcion con la formula x^2 + x + 1
 
+// - definir una funcion con la formula x^2 + x + 1
 
-- definir una funcion con la formula libre plolinomio,
-
-
-reto: 
-- artitmetico, calculadora de raices de una parabola 
-una parabola se define con la formuna ax^2 + bx + c = 0
-(-b +- (b^2 - 4*ac)^(1/2)) / 2a 
-*/
 
 function resolver(x){
 
@@ -24,3 +15,35 @@ function resolver(x){
 x = parseInt(window.prompt("Ingresa el numero que quieres calcular en la funcion la funciones x^2 + x + 1 es: "))
 
 console.log( " es : " + resolver(x) ) 
+
+
+// Ejercicio 2
+// - definir una funcion con la formula libre plolinomio,
+     
+
+// Polinomio P(x) = 2x^3+ 5x - 3, https://www.superprof.es/apuntes/escolar/matematicas/algebra/polinomios/polinomios.html
+
+function Polinomio(n){
+ 
+    n = Math.pow(n, 3)
+    ecuacion = (( 2 * n) + (5 * n) - 3 )
+
+    return ecuacion
+
+}
+n = parseInt(window.prompt("Ingresa el numero que quieres calcular en la funcion la funciones 2x^3+ 5x - 3: "))
+
+console.log( " es : " + Polinomio(n) ) 
+
+
+/*
+
+
+reto: 
+- artitmetico, calculadora de raices de una parabola 
+una parabola se define con la formuna ax^2 + bx + c = 0
+(-b +- (b^2 - 4*ac)^(1/2)) / 2a 
+*/
+
+
+
